6a19bbc15e636354aabd32a66c352b932b698a7b
Albin Ljunghusen <albinljunghusen@gmail.com>
2019-03-27 15:09:26 +0100

    Fixed direction of /app

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import AuctionList from './AuctionList';
-import AuctionListItem from './AuctionListItem';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import AuctionList from "./AuctionList";
+import AuctionListItem from "./AuctionListItem";
 import Navigation from "./Navigation";
 
 class App extends Component {
   render() {
-    let auctionListItem = <AuctionListItem />
+    let auctionListItem = <AuctionListItem />;
     let navigation = <Navigation />;
-    let auctionList = <AuctionList />
+    let auctionList = <AuctionList />;
 
-    return <div className="App">
-    {auctionListItem}
-    {navigation}
-    {auctionList}
-    </div>;
-   
+    return <div className="App">{/* soda */}</div>;
   }
 }
 
