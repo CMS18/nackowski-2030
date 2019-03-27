accab34a46e1818380be80b7aee3b77c3c2eed64
sara <sara.berling@hotmail.com>
2019-03-27 15:19:07 +0100

    Removed some imports from App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,28 @@ import "./App.css";
 import AuctionList from "./AuctionList";
 import AuctionListItem from "./AuctionListItem";
 import Navigation from "./Navigation";
+import CreateNewAuction from './CreateNewAuction';
 
 class App extends Component {
   render() {
     let auctionListItem = <AuctionListItem />;
     let navigation = <Navigation />;
+<<<<<<< Updated upstream
     let auctionList = <AuctionList />;
 
     return <div className="App">{/* soda */}</div>;
+=======
+    let auctionList = <AuctionList />
+    let newAuction = <CreateNewAuction name="Sara" />
+
+    return <div className="App">
+    {auctionListItem}
+    {navigation}
+    {auctionList}
+    {newAuction}
+    </div>;
+   
+>>>>>>> Stashed changes
   }
 }
 
