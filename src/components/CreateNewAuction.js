37da6f97e8f0409a240597ae9f6a5bfe8fbfb0e9
sara <sara.berling@hotmail.com>
2019-03-29 09:36:43 +0100

    Updated the CreateNewAuction component

--- src/components/CreateNewAuction.js
+++ src/components/CreateNewAuction.js
@@ -1,29 +1,38 @@
 import React, { Component } from 'react';
 
 export default class CreateNewAuction extends Component {
+
+    state = {
+        name: null,
+        title: null,
+        endDate: null,
+        estimate: null,
+        info: null
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.id]: e.target.value
+        });
+    }
+    
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+    }
+
     render() {
-
-        return (<div>
-            <label>{this.props.name}</label>
-            <br />
-            <label>Rubrik</label>
-            <br />
-            <input type="text" />
-            <br />
-            <label>Slutdatum</label>
-            <br />
-            <input type="text" class="datepicker"></input>
-            <br />
-            <label>Utropspris</label>
-            <br />
-            <input type="text" />
-            <br /><br />
-            <label>Information om produkten</label>
-            <br />
-            <textarea cols="70" rows="20"></textarea>
-            <br />
-            <button id="createNewCMD" onClick={this.props.onClick}>Spara auktion</button>
-
-        </div>)
+        return (<form onSubmit={this.handleSubmit}>
+            <label htmlFor="name">{this.props.name}</label>
+            <label htmlFor="title">Rubrik</label>
+            <input type="text" id="title" onChange={this.handleChange} />
+            <label htmlFor="endDate">Slutdatum</label>
+            <input type="text" class="datepicker" id="endDate" onChange={this.handleChange}></input>
+            <label htmlFor="estimate">Utropspris</label>
+            <input type="text" id="estimate" onChange={this.handleChange} />
+            <label htmlFor="info">Information om produkten</label>
+            <textarea cols="70" rows="20" id="info" onChange={this.handleChange}></textarea>
+            <button id="createNewCMD">Spara auktion</button>
+            </form>)
     }
 }
