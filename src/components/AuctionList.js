0d70a80bd08f2b4460dbbe63a1fe6cf27d570758
Hannibal Ftaw <Hannibal_rebok@hotmail.com>
2019-03-29 11:29:53 +0100

    Updated AuctionList with Fetch function

--- src/components/AuctionList.js
+++ src/components/AuctionList.js
@@ -1,34 +1,59 @@
 import React, {Component} from 'react';
 // import {connect} from 'react-redux';
-import AuctionListItem from './AuctionListItem';
+import CreateNewAuction from './CreateNewAuction';
 
 //testcode start ---------------------------
+
 export default class AuctionList extends Component{
 
-    constructor(props){
+    constructor(){
+        super();
 
-        super(props);
-        this.state = {posts:[]};
-
+        this.state = {
+            'items': []
+        }
     }
 
     componentDidMount(){
-        fetch('http://nackowskis.azurewebsites.net/api/Auktion/2030/') //skickar anropet till API:et
-        .then(response => response.json()) //Hämtar json datan
-        .then(json => this.setState({posts: json})) //Skickar in datan till statet alternativt consollen
+        this.getItems();
     }
 
-    // let promise = await fetch(url);
-    // let data = await promise.json();
-    // this.setState({posts:data});
+    getItems(){
+        fetch('http://nackowskis.azurewebsites.net/api/Auktion/2030/')
+        .then(results => results.json())
+        .then(json => json.results)
+        .then(results => this.setState({'items': results}));
+    }
 
+    addAuction = (auction) => {
+        auction.id = Math.random();
+        let auctions = [...this.state.auctions]
+        this.setState({
+            auctions
+        })
 
+    }
 
-    render()
-        {
-        return <AuctionList posts = {this.state.posts} />
-        }
+    render(){
+        return(
+            <ul>
+                {this.state.items.map(function(item, index){
+                    return <div key={index} className="">
+                        <h1>{item.title}</h1>
+                        <p>{item.description}</p>
+                        <p>{item.expiredate}</p>
+                        <p>{item.bid}</p>
+                        {/* <CreateNewAuction createNewAuction{this.createNewAuction} /> */}
+
+                    </div>
+                    }
+                 )}
+
+            </ul>
+        );
+    }
 
 
 }
+
 //testcode end -----------------------------
