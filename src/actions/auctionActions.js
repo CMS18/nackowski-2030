b2156efd8cef30ed20dd54fe96cf9e7f46d810cb
Albin Ljunghusen <albinljunghusen@gmail.com>
2019-04-02 12:01:03 +0200

    Added addAuction functionality in auctionactions

--- src/actions/auctionActions.js
+++ src/actions/auctionActions.js
@@ -5,15 +5,33 @@ export const loadAuctions = () => {
     axios
       .get('http://nackowskis.azurewebsites.net/api/Auktion/1040/')
       .then(res => {
-        console.log(res);
         dispatch({ type: 'LOAD_AUCTIONS', payload: { auctions: res.data } });
       });
   };
 };
 
-export const addAuction = (auction) => {
-  return {
-    type: "ADD_AUCTION",
-    auction
-  }
-}
+export const addAuction = auction => {
+  let fakeAuction = {
+    Titel: 'Strut',
+    Beskrivning: 'Glassig strut',
+    StartDatum: '2018-08-12T10:42:00',
+    SlutDatum: '2018-09-12T10:42:00',
+    Gruppkod: 2030,
+    Utropspris: 100,
+    SkapadAv: 'Albin'
+  };
+
+  return (dispatch, getState) => {
+    axios({
+      method: 'POST',
+      url: 'http://nackowskis.azurewebsites.net/api/Auktion/2030/',
+      headers: {
+        Accept: 'application/json, text/plain, */*',
+        'Content-Type': 'application/json'
+      },
+      data: JSON.stringify(fakeAuction)
+    }).then(res => {
+      dispatch({ type: 'ADD_AUCTION', payload: { auction: auction } });
+    });
+  };
+};
--- src/reducers/auctionReducer.js
+++ src/reducers/auctionReducer.js
@@ -1,10 +1,13 @@
-export default (state = {}, { type, payload }) => {
+export default (state = [], { type, payload }) => {
   switch (type) {
     case 'LOAD_AUCTIONS':
-      return { ...state, ...payload };
+      let { auctions } = payload;
+      return { ...state, auctions };
 
-    case "ADD_AUCTION":
-      return { ...state, ...payload };
+    case 'ADD_AUCTION':
+      let { auction } = payload;
+
+      return { ...state, auctions: [...state.auctions, auction] };
 
     default:
       return state;
